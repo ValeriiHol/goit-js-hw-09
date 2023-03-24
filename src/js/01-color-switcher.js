d23a6bff44c75815118a01023b006f61cef350c1
ValeriiHol <rielty@gmail.com>
2023-03-24 12:56:14 +0200

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const bodyColor = document.querySelector('body');
+const startBtnClick = document.querySelector('[data-start]');
+const stopBtnClick = document.querySelector('[data-stop]');
 
+let timeInterval = null;
+
+getRandomHexColor = () => {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+backgroundChangeColor = () => {
+    const newColor = getRandomHexColor();
+    bodyColor.style.backgroundColor = newColor;
+}
+
+startBtnClick.addEventListener('click', () => {
+    timeInterval = setInterval(backgroundChangeColor, 1000);
+    startBtnClick.disabled = true;
+    stopBtnClick.disabled = false;
+})
+
+stopBtnClick.addEventListener('click', () => {
+    clearInterval(timeInterval);
+    startBtnClick.disabled = false;
+    stopBtnClick.disabled = true;
+})
