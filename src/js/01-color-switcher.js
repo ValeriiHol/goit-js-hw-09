5142691da9389296c981ae5ec34c49cca75deed9
ValeriiHol <rielty@gmail.com>
2023-03-27 14:13:16 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,21 +5,21 @@ const stopBtnClick = document.querySelector('[data-stop]');
 let timeInterval = null;
 stopBtnClick.disabled = true;
 
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
-// getRandomHexColor = () => {
-//     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-
-function backgroundChangeColor () {
-    const newColor = getRandomHexColor();
-    bodyColor.style.backgroundColor = newColor;
+// function getRandomHexColor() {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+//   }
+const getRandomHexColor = () => {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-// backgroundChangeColor = () => {
+
+// function backgroundChangeColor () {
 //     const newColor = getRandomHexColor();
 //     bodyColor.style.backgroundColor = newColor;
 // }
+const backgroundChangeColor = () => {
+    const newColor = getRandomHexColor();
+    bodyColor.style.backgroundColor = newColor;
+}
 // нажаль стрілочні функціі чомусь працюють в ВСкоде, а в Хромі видають помилки
 
 startBtnClick.addEventListener('click', () => {
