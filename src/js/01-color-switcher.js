aa4e058e9ea6239bc979cf69ccd9d83513aab049
ValeriiHol <rielty@gmail.com>
2023-03-27 13:46:37 +0300

    remade task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,9 +4,13 @@ const stopBtnClick = document.querySelector('[data-stop]');
 
 let timeInterval = null;
 
-getRandomHexColor = () => {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+// getRandomHexColor = () => {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
 
 backgroundChangeColor = () => {
     const newColor = getRandomHexColor();
